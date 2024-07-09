8d12ceadfcccfcf66df92537c0fed0c0de334461
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-07-09 15:27:00 +0100

    feat(frontend): reset password page

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -1,5 +1,7 @@
 // src/components/ResetPassword.jsx
 import React from "react";
+import Header from "./atoms/header";
+import { Link } from "react-router-dom";
 
 const ResetPassword = () => {
   const handleSubmit = (e) => {
@@ -9,17 +11,26 @@ const ResetPassword = () => {
   };
 
   return (
-    <div>
-      <h2>Réinitialiser le mot de passe</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Réinitialiser
-        </button>
-      </form>
+    <div className="flex flex-col pt-4 h-full">
+      <Header />
+      <div className="flex flex-col items-center bg-[#9CD8ED] w-fit p-24 self-center justify-self-center rounded-3xl gap-4">
+        <h2 className="text-6xl">Réinitialiser le mot de passe</h2>
+        <form
+          className="flex flex-col gap-1 w-96 items-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="form-group w-full ">
+            <label htmlFor="email">Email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary bg-[#11A86D] border-0 hover:bg-[#29CD8D]"
+          >
+            Réinitialiser
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
