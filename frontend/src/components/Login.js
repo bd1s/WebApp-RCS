5b1a53f2406cf8b691e7060f486178b9ad2c27be
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-07-09 14:57:47 +0100

    feat(frontend): login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,30 +1,37 @@
 // src/components/Login.jsx
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "./atoms/header";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Connexion</h2>
-      <form>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" className="form-control" id="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Mot de passe</label>
-          <input type="password" className="form-control" id="password" />
-        </div>
-        <button type="submit" className="btn btn-primary mr-2">
-          Se connecter
-        </button>
-        <Link to="/register" className="btn btn-secondary mr-2">
-          Créer un compte
-        </Link>
-        <Link to="/reset-password" className="btn btn-link">
-          Réinitialiser le mot de passe
-        </Link>
-      </form>
+    <div className="flex flex-col pt-4 h-full">
+      <Header />
+      <div className="flex flex-col items-center bg-[#9CD8ED] w-fit p-24 self-center justify-self-center rounded-3xl gap-4">
+        <h2 className="text-6xl">Connexion</h2>
+        <form className="flex flex-col gap-1 w-96 items-center">
+          <div className="form-group w-full">
+            <label htmlFor="email">Email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group w-full">
+            <label htmlFor="password">Mot de passe</label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary bg-[#11A86D] border-0 hover:bg-[#29CD8D]"
+          >
+            Se connecter
+          </button>
+          <Link to="/reset-password" className="underline">
+            Mot de passe oublié ?
+          </Link>
+          <Link to="/register" className="underline">
+            Pas encore inscrit ? Créez un compte
+          </Link>
+        </form>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/components/atoms/header.js
@@ -0,0 +1,16 @@
+// src/components/Login.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import fac_logo from "../../assets/fac_logo.png";
+import uni_logo from "../../assets/uni_logo.png";
+
+const Header = () => {
+  return (
+    <header className="flex flex-row justify-between">
+      <img src={fac_logo} alt="fac_logo" className="w-36 h-36" />
+      <img src={uni_logo} alt="uni_logo" className="h-36" />
+    </header>
+  );
+};
+
+export default Header;
