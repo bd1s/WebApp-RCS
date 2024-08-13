6a7d05065052252a3c6c7be990092dc86ec694a0
aL0NEW0LF <mohamedfakhreeddine2019@gmail.com>
2024-08-13 16:06:06 +0100

    build: removed node modules

--- backend/node_modules/@syncfusion/ej2-react-schedule/src/schedule/index.js
+++
@@ -1,5 +0,0 @@
-export * from './views-directive';
-export * from './resources-directive';
-export * from './headerrows-directive';
-export * from './toolbaritems-directive';
-export * from './schedule.component';
